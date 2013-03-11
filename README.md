3aa0d3e81ae0c6138cd67e9fc524aa5f48a1eac1
ivoarch <ivkuzev@gmail.com>
2013-03-11 12:31:02 +0100

    Update Readme && Added some useful information to the config file

--- README.md
+++ README.md
@@ -1,34 +1,40 @@
 AntiWM - README
 ---------------------
-
+ 
 About
------
+---------
 AntiWM is a simple window manager inspired by ratpoison, which was inspired by GNU screen. 
 All interaction with the window manager is done through the following keystrokes.
 
-C-t C-c
+`C-t C-c`
 This opens a new terminal session.
 
-C-t C-e
+`C-t C-e`
 This opens a new editor session.
 
-C-t C-w
+`C-t C-w`
 This opens the program bar, which displays currently running windows. 
 Jump to each window by number with 'C-t C-<n>, where <n> is the number of the window.
 
-C-t C-p
+`C-t C-m` 
+This opens the menu program.
+
+`C-t C-b` 
+This run banish command.
+
+`C-t C-p`
 This jumps to the previous window in the window list.
 
-C-t C-n
+`C-t C-n`
 This jumps to the next window in the window list.
 
-C-t C-<n>
+`C-t C-<n>`
 This jumps to a window by number as shown in the Program Bar.
 
-C-t C-t
+`C-t C-t`
 This toggles between the current window and the last window.
 
-C-t t
+`C-t t`
 Sends C-t to the current window.
 
 NOTE: These are the default settings. There is no antiwm.conf or antirc to edit. 
@@ -39,13 +45,19 @@ Building
 AntiWM comes with a Makefile that should work in most cases.
 If you want to change any part of the make/install process, or if there is trouble compiling, try and edit it.
 
-1) cd /path/to/antiwm/src
+    cd /path/to/antiwm/src
+ 
 The path should point to where AntiWM was extracted.
-2) make
+ 
+    make
+ 
 This compiles the program.  
 Most warnings are fine, but errors should be cause to check the Makefile. 
-3) make install
-This installs AntiWM.  By default AntiWM installs to /usr/local/bin.
+
+    make install
+
+This installs AntiWM.   
+By default AntiWM installs to /usr/local/bin.
 If this is not where AntiWM should be installed, change the path indicated in the Makefile. 
 NOTE: This step should be done as a priviledged user.
 
--- conf.h
+++ conf.h
@@ -1,21 +1,27 @@
 /* Configure AntiWM here, recompile, and install. */
-#define KEY_PREFIX      't'
-#define MODIFIER_PREFIX ControlMask
-#define KEY_TERM       'c'
-#define KEY_EDIT       'e'
-#define KEY_MENU       'm'
-#define KEY_BANISH     'b'
-#define KEY_PREVWINDOW  'p'
-#define KEY_NEXTWINDOW  'n'
-#define KEY_LASTWINDOW  't'	/* toggle current and previous windows */
-#define KEY_TOGGLEBAR   'w'	/* toggle display of the bar */
-#define TERM_PROG      "xterm"	/* launch a terminal */
-#define EDIT_PROG      "vim"	/* launch an editor */
+
+/* key definitions */
+#define KEY_PREFIX      't'          /* prefix key */
+#define MODIFIER_PREFIX ControlMask  /* modifier prefix */
+#define KEY_TERM        'c'          /* launch a terminal */
+#define KEY_EDIT        'e'          /* launch an editor */
+#define KEY_MENU        'm'          /* launch a menu program */
+#define KEY_BANISH      'b'          /* sends the cursor to the bottom right corner of the screen */
+#define KEY_PREVWINDOW  'p'          /* focus previous window */
+#define KEY_NEXTWINDOW  'n'          /* focus next windows */
+#define KEY_LASTWINDOW  't'	     /* toggle current and previous windows */
+#define KEY_TOGGLEBAR   'w'	     /* toggle display of the bar */
+
+/* default commands */
+#define TERM_PROG      "xterm"
+#define EDIT_PROG      "vim"
 #define MENU_PROG      "dmenu_run"
-#define BAR_FG_COLOR    "Gray60"
-#define BAR_BG_COLOR    "Lightgreen"
-#define BAR_BOLD_COLOR  "Black" /* indicate current window */
-#define FONT_NAME       "fixed"	/* font to use */
-#define BAR_PADDING     1	/* top and bottom bar padding  */
-#define BAR_LOCATION    1	/* 0=bottom-left 1=top-left 2=bottom-right 3=top-right */
-#define BAR_TIMEOUT     5	/* time before bar autohides 0=don't autohide */
+
+/* appearance */
+#define BAR_FG_COLOR    "Gray60"     /* foreground color */
+#define BAR_BG_COLOR    "Lightgreen" /* bacground color */
+#define BAR_BOLD_COLOR  "Black"      /* indicate current window */
+#define FONT_NAME       "fixed"	     /* font to use */
+#define BAR_PADDING     1	     /* top and bottom bar padding  */
+#define BAR_LOCATION    1	     /* 0=bottom-left 1=top-left 2=bottom-right 3=top-right */
+#define BAR_TIMEOUT     5	     /* time before bar autohides 0=don't autohide */
